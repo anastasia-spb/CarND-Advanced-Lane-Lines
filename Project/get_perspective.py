d12e5911be6d45942992876f5958a430c83c431b
Luxoft ANASTASIYA-VLASOVA <luxoft.anastasiya-vlasova.ext@valeo.com>
2019-04-19 22:01:51 +0300

    Perspektive results stored in form of npy

--- Project/get_perspective.py
+++ Project/get_perspective.py
@@ -80,7 +80,8 @@ def align_manually_chosen_points(points, offset_top=0, offset_bottom=0):
     src = np.float32(points_list)
     return src, points_list
 
-def roi(img,vertices):
+
+def roi(img, vertices):
     # blank mask
     mask = np.zeros_like(img)
     # filling pixels inside the polygon defined by vertices with the fill color
@@ -89,13 +90,14 @@ def roi(img,vertices):
     masked_background = cv2.bitwise_or(img, mask)
     return masked_background
 
+
 def plot_mask_on_image(src_img, points_list):
     vertices = np.array([points_list], dtype=np.int32)
     masked_img = roi(src_img, vertices)
     return masked_img
 
 
-def set_destination_points(input_img, offset_x=100, offset_y=300):
+def set_destination_points(input_img, offset_x=50, offset_y=400):
     [x, y, z] = input_img.shape
     dst = np.float32(
         [[offset_x, offset_y], [x - offset_x, offset_y], [x - offset_x, y], [offset_x, y]])
@@ -103,9 +105,16 @@ def set_destination_points(input_img, offset_x=100, offset_y=300):
 
 
 def get_perpective_matrix():
-    M = np.array([[-4.13308420e-01, -8.37005327e-01, 6.15526118e+02],
-                  [-1.64763533e-15, -3.83969657e+00, 1.84070725e+03],
-                  [-1.33349842e-18, -2.38795772e-03, 1.00000000e+00]])
+    M = np.array([[-3.80405332e-01, -8.38842527e-01, 5.90145226e+02],
+                  [1.28097845e-15, -3.14186626e+00, 1.39438821e+03],
+                  [9.66453255e-19, -2.33011813e-03, 1.00000000e+00]])
+    return M
+
+
+def get_perpective_matrix_for_strong_curved():
+    M = np.array([[-5.16084489e-01, -8.34969471e-01, 6.02817752e+02],
+                  [0.00000000e+00, -4.26114911e+00, 2.18963084e+03],
+                  [8.61287813e-19, -2.31935964e-03, 1.00000000e+00]])
     return M
 
 
@@ -116,6 +125,7 @@ def calculate_perpective_matrix(input_img, calculate_matrix=False):
         result = plot_mask_on_image(input_img, points_list)
         dst = set_destination_points(input_img)
         M = cv2.getPerspectiveTransform(src, dst)
+        print(M)
     else:
         M = get_perpective_matrix()
         result = input_img
@@ -128,34 +138,51 @@ def visualize(img_ref, warped):
     ax1.set_title('Original image')
     ax1.imshow(img_ref)
 
+    img_ref_with_line = np.copy(warped)
     lineThickness = 2
     x = 50
-    cv2.line(warped, (x, 0), (x, img_ref.shape[0]), (0, 255, 0), lineThickness)
+    cv2.line(img_ref_with_line, (x, 0), (x, img_ref.shape[0]), (0, 255, 0), lineThickness)
 
     ax2.set_title('Transformed image')
-    ax2.imshow(warped)
+    ax2.imshow(img_ref_with_line)
 
     plt.show()
 
     return
 
+
 def save_binary_image(img, name_dir, img_name):
     directory = '../test_images/' + name_dir
     if not os.path.exists(directory):
         os.makedirs(directory)
-    directory = directory + '/' + img_name
-    cv2.imwrite(directory, img.astype('uint8') * 255)
+    img_name_no_ext = os.path.splitext(img_name)[0]
+    directory = directory + '/' + img_name_no_ext + '.npy'
+    np.save(directory, img)
+
+
+def load_and_visu_stored_arrays():
+    arrays_names = glob.glob('../test_images/warped/test*.npy')
+    destination_dir_name = "warped"
+    for fname in arrays_names:
+        img = np.load(fname)
+        plt.imshow(img)
+        plt.show()
+
 
 def main():
-    images_names = glob.glob('../test_images/combined_binary/test*.jpg')
-    destination_dir_name = "warped"
-    for fname in images_names:
-        img = cv2.imread(fname)
-        M, masked_img = calculate_perpective_matrix(img, calculate_matrix = True)
-        img_size = (img.shape[1], img.shape[0])
-        warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
-        visualize(masked_img, warped)
-        save_binary_image(warped, destination_dir_name, ntpath.basename(fname))
+    show_results = True
+    if show_results == False:
+        images_names = glob.glob('../test_images/combined_binary/test*.jpg')
+        destination_dir_name = "warped"
+        for fname in images_names:
+            img = cv2.imread(fname)
+            M, masked_img = calculate_perpective_matrix(img, calculate_matrix=True)
+            img_size = (img.shape[1], img.shape[0])
+            warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+            visualize(masked_img, warped)
+            save_binary_image(warped, destination_dir_name, ntpath.basename(fname))
+    else:
+        load_and_visu_stored_arrays()
 
 
 if __name__ == '__main__':
